e707fc32f9a9251bb8be509ce886d6b9a971081a
Soralith <ariqrafifkomaragit@gmail.com>
2026-08-12 01:37:25 +0700

    Login and register.

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
+    return render(request, 'dashboard/pages/login.html')
+
+def dashboard(request):
     return render(request, 'dashboard/pages/dashboard.html')
